714345d912316c1c708320beb9d0d7a3759cbff3
MiguelRodriguesSilva <rm92358@estudante.fieb.edu.br>
2023-08-03 01:50:43 -0300

    Miguel - Sem Nota - 03/08/2023 01:50

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float velocidadePlayer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,23 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovimentacaoPlayer();
+    }
+
+    void MovimentacaoPlayer()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            transform.Translate(Vector3.right * velocidadePlayer * horizontal * Time.deltaTime);
+        }
+
+        if (Input.GetAxisRaw("Vertical") != 0)
+        {
+            transform.Translate(Vector3.up * velocidadePlayer * vertical * Time.deltaTime);
+        }
         
     }
 }

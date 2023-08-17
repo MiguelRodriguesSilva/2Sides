274458a46754147fb402f37429460580fea6f218
MiguelRodriguesSilva <rm92358@estudante.fieb.edu.br>
2023-08-17 01:10:15 -0300

    Davilas & Miguel - Correcao bug Script - 17/08/2023 01:10

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -43,18 +43,10 @@ public class Player : MonoBehaviour
 
 
         if (horizontal != 0)
-<<<<<<< HEAD
-=======
         {
             animador.SetBool("estaAndando", true);
             animador.SetBool("estaVirado", true);
 
-        }
-        else if (vertical != 0)
->>>>>>> 2b8b70951938d78dc353e656a1e5afd71cdbe613
-        {
-            animador.SetBool("estaAndando", true);
-            animador.SetBool("estaVirado", true);
         }
         else if (vertical != 0)
         {
@@ -64,14 +56,8 @@ public class Player : MonoBehaviour
                 animador.SetBool("estaAndando", true);
                 animador.SetBool("estaVirado", false);
             }
-<<<<<<< HEAD
-        }
-        else if (vertical == 0 || horizontal == 0)
-=======
-           
         }
         else if (animador.GetBool("estaVirado") == false || vertical == 0 || horizontal == 0)
->>>>>>> 2b8b70951938d78dc353e656a1e5afd71cdbe613
         {
             animador.SetBool("estaAndando", false);
             if (animador.GetBool("estaVirado") == true)
@@ -80,17 +66,8 @@ public class Player : MonoBehaviour
             }
         }
 
-<<<<<<< HEAD
-        if (horizontal > 0 && animador.GetBool("estaVirado") == true)
-        {
-            sr.flipX = true;
-        }
-        else
-=======
-        
 
         if (horizontal <= 0 || animador.GetBool("estaVirado") == false)
->>>>>>> 2b8b70951938d78dc353e656a1e5afd71cdbe613
         {
             sr.flipX = false;
         }
@@ -99,9 +76,7 @@ public class Player : MonoBehaviour
             sr.flipX = true;
         }
 
-
     }
-
     public void Dano(int danoInimigo)
     {
         vidaPlayer -= danoInimigo;
@@ -110,13 +85,11 @@ public class Player : MonoBehaviour
         {
             vidaPlayer = 0;
             MortePlayer();
+
         }
     }
-
     void MortePlayer()
     {
         Debug.Log("To morto");
     }
 }
-
-
